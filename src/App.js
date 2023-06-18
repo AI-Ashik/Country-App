3cfe8d104cab95191e108d82055df0bc78fd2721
AI-Ashik <arifulislamashik07@gmail.com>
2023-06-18 21:32:48 +0600

    country api calling using useEffect Hook

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Countries from './Components/Countries';
+
+const url = 'https://restcountries.com/v3.1/all';
+
+const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [countries, setCountries] = useState([]);
+
+    const fetchData = async (url) => {
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setCountries(data);
+            setIsLoading(false);
+            setError(null);
+        } catch (error) {
+            setIsLoading(false);
+            setError(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData(url);
+    }, []);
+
+    return (
+        <>
+            <h1>Country App</h1>
+            {isLoading && <h2>Loading...</h2>}
+            {error && <h2>{error.message}</h2>}
+            {countries && <Countries countries={countries} />}
+        </>
+    );
+};
+
+export default App;
